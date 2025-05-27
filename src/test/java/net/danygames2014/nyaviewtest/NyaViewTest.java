20561f5b6167ea33797e98980c87b8a29e1f5f14
Daniel Fildán <fildand@centrum.cz>
2025-05-27 12:59:55 +0200

    Use new search in test

--- src/test/java/net/danygames2014/nyaviewtest/NyaViewTest.java
+++ src/test/java/net/danygames2014/nyaviewtest/NyaViewTest.java
@@ -1,9 +1,14 @@
 package net.danygames2014.nyaviewtest;
 
 import net.danygames2014.nyaview.NyaView;
+import net.danygames2014.nyaview.mapping.entry.ClassMappingEntry;
 import net.danygames2014.nyaview.search.OldSearch;
+import net.danygames2014.nyaview.search.Search;
+import net.danygames2014.nyaview.search.SearchParameters;
+import net.danygames2014.nyaview.search.SearchResult;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 import static net.danygames2014.nyaview.search.OldSearch.parseSearchQuery;
@@ -18,23 +23,39 @@ public class NyaViewTest {
         Scanner sc = new Scanner(System.in);
         while (run) {
             String input = sc.nextLine();
-            OldSearch.SearchParameters parameters = parseSearchQuery(input);
+
+            SearchParameters parameters = SearchParameters.parse(input);
+            
             if (parameters == null) {
-                System.err.println("Invalid Search Query");
+                System.err.println("Invalid search query");
                 continue;
             }
 
             System.out.println(parameters);
+            SearchResult result = Search.search(parameters);
 
-            ArrayList<String> result = search(parameters);
-            if (result.isEmpty()) {
-                System.err.println("No Results");
-            } else {
-                for (var item : result) {
-                    System.out.println(item);
-                }
-                System.out.println("Found " + result.size() + (result.size() > 1 ? " results" : " result"));
+            for (Map.Entry<ClassMappingEntry, SearchResult.SearchResultClassEntry> r : result.results.entrySet()) {
+                //System.out.println(r.getKey());
             }
+
+            System.out.println("Found " + result.methodCount + " methods and " + result.fieldCount + " fields in " + result.results.size() + (result.results.size() > 1 ? " classes" : " class"));
+//            OldSearch.SearchParameters parameters = parseSearchQuery(input);
+//            if (parameters == null) {
+//                System.err.println("Invalid Search Query");
+//                continue;
+//            }
+//
+//            System.out.println(parameters);
+//
+//            ArrayList<String> result = search(parameters);
+//            if (result.isEmpty()) {
+//                System.err.println("No Results");
+//            } else {
+//                for (var item : result) {
+//                    System.out.println(item);
+//                }
+//                System.out.println("Found " + result.size() + (result.size() > 1 ? " results" : " result"));
+//            }
         }
     }
 }
