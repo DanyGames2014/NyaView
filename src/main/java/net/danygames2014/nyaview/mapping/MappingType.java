8f48cca0e8157bcfefd72860999c83bbd3be202d
Daniel Fildán <fildand@centrum.cz>
2025-06-05 10:49:57 +0200

    Begone intermediary "mapping" type

--- src/main/java/net/danygames2014/nyaview/mapping/MappingType.java
+++ src/main/java/net/danygames2014/nyaview/mapping/MappingType.java
@@ -3,8 +3,6 @@ package net.danygames2014.nyaview.mapping;
 public enum MappingType {
     // Obfuscated -> Named
     MCP,
-    // Obfuscated -> Intermediary
-    INTERMEDIARY,
     // Intermediary -> Named
     BABRIC;
 }
